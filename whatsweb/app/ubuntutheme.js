c0ae732d79fced1024a553f4395f691e72c016e6
Merkhad Luigton <fusekai@outlook.com>
2020-11-27 00:25:57 -0300

    move the back button back into the headerbar

--- whatsweb/app/ubuntutheme.js
+++ whatsweb/app/ubuntutheme.js
@@ -122,7 +122,7 @@ function menu(){
     newHTML.style = "";
     newHTML.innerHTML   = "<a href='#' onclick=\"document.getElementById('app').getElementsByClassName('two')[0].childNodes[3].style.display = 'none'; document.getElementById('app').getElementsByClassName('two')[0].childNodes[2].style.display = 'block'; \"><span data-icon='left'><svg class='svg_back' id='Layer_1' xmlns='http://www.w3.org/2000/svg' viewBox='0 0 21 21' width='21' height='21'><path fill='#263238' fill-opacity='.33' d='M4.8 6.1l5.7 5.7 5.7-5.7 1.6 1.6-7.3 7.2-7.3-7.2 1.6-1.6z'></path></svg></span></a>";
 
-    var eElement = document.getElementById("app").getElementsByClassName('copyable-area')[0];
+    var eElement = document.getElementById("main").childNodes[1];
     eElement.insertBefore(newHTML, eElement.firstChild);
 
     check = check + 1;
